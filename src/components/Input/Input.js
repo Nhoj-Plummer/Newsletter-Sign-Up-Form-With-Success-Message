2ca2fe40a9e1220b39e986c14dc1c7bda0b027f8
Nhoj <nhojplummer@yahoo.com>
2023-09-12 21:53:21 -0400

    Making progress

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,7 +3,6 @@ import "./Input.css"
 import Form from "../Form/Form"
 import Mobile from "../../assets/illustration-sign-up-mobile.svg"
 import Desktop from "../../assets/illustration-sign-up-desktop.svg"
-import Icon from "../../assets/icon-list.svg"
 
 const textList = [{ text: "Product discovery and building what matters" }, { text: "Measuring to ensure updates are a success" }, { text: "And much more!" }]
 
@@ -24,7 +23,7 @@ const Input = () => {
         <form>
           <p>Email Address</p>
           <div className="input-container_text-container-form">
-            <input type="email" name="email" placeholder="Email Address" required />
+            <input type="email" name="email" placeholder="email@company.com" required />
           </div>
           <button type="submit">Subscribe to monthly newsletter</button>
         </form>
