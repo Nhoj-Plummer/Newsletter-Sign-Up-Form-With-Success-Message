82be3f750bc0016ba7ab5eb248f6690bf01157fb
Nhoj <nhojplummer@yahoo.com>
2023-09-14 16:28:33 -0400

    Trying to make progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import "./App.css"
+import { useState } from "react"
 import { Input, Output } from "./components/index"
 
-
 function App() {
-
-
+  const [email, setEmail] = useState(null)
   return (
     <>
       <Input />
--- src/components/Output/Output.js
+++ src/components/Output/Output.js
@@ -1,16 +1,15 @@
 import React from "react"
-import Form from "../Form/Form"
 import "./Output.css"
 import Success from "../../assets/icon-success.svg"
 
-const Output = email => {
+const Output = ({ email }) => {
   return (
     <>
       <div className="output-container section-padding">
         <img src={Success} alt="success" />
         <h1>Thanks for subscribing!</h1>
         <p>
-          A confirmation email has been sent to <span>ash@loremcomapny.com</span>. Please open it and click the button inside to confirm your subscription.
+          A confirmation email has been sent to <span>{email}</span>. Please open it and click the button inside to confirm your subscription.
         </p>
         <button>Dismiss message</button>
       </div>
